5ca9947ab7f5ca5ec0a262c48a623ae99789e2a1
RafaelMath <rgildo38@gmail.com>
2020-04-16 14:01:04 -0300

    init stopwatch

---
+++ js/temporizador.js
@@ -0,0 +1,49 @@
+
+        let acomulador = 0
+        let clear
+       
+        function calcFormatTimer(count) {
+            
+                let millesimal, seconds, minutes, hours
+                
+                millesimal = ("00" + Math.floor(count % 100)).slice(-2)
+                
+                count /= 100
+                seconds = ("00" + Math.floor(count % 60)).slice(-2)
+                
+                count /= 60
+                minutes = ('00' + Math.floor(count % 60)).slice(-2)
+                
+                count /= 60
+                hours = ('00' + Math.floor(count % 24)).slice(-2)
+            
+            return `${hours}:${minutes}:${seconds}.${millesimal}`
+        }
+
+        function timer() {
+                
+            $('#timer').text( calcFormatTimer( acomulador ))
+            acomulador++
+        }
+        
+        $('#start').click((e) => {
+            if ($(e.target).text() === 'START') {
+                
+                $(e.target).text('STOP').css('background', 'rgb(216, 22, 22)')
+                clear = setInterval(timer, 10)
+            }
+            else {
+                
+                $( e.target ).text( 'START' ).css('background','rgb(56, 209, 82)')
+                clearInterval( clear ) 
+            }
+        })
+
+        $('#restart').click( (e) => {
+            clearInterval( clear )
+            $('#timer').text('00:00:00.00')
+            acomulador = 0
+        })
+
+
+        
